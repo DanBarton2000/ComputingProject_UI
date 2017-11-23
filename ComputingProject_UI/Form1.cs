4c6e28a6437ff3e835dcd9562e30dc661f116a2f
DanBarton2000 <danbarton2000@gmail.com>
2017-11-23 10:43:29 +0000

    Collision code

--- ComputingProject_UI/Form1.cs
+++ ComputingProject_UI/Form1.cs
@@ -33,7 +33,7 @@ namespace ComputingProject_UI
         TimeController timeController;
 
         Vector centre;
-        QuadTree<CelestialObject> screen;
+        QuadTree<IQuadtreeObject> screen;
 
         public Form1()
         {
@@ -52,12 +52,12 @@ namespace ComputingProject_UI
 
             ScreenBounds();
 
-            ObjectManager<IQuadtreeObject>.SetScreenBounds(new Vector(screenWidth, screenHeight));
+            ObjectManager.SetScreenBounds(new Vector(screenWidth, screenHeight));
 
             AddObjects();
 
             // Add all objects at the start of the simualtion to the quadtree.
-            foreach (CelestialObject obj in ObjectManager<IQuadtreeObject>.AllObjects) {
+            foreach (CelestialObject obj in ObjectManager.AllObjects) {
                 screen.Insert(obj);
             }
         }
@@ -76,7 +76,7 @@ namespace ComputingProject_UI
         {
             base.OnPaint(e);
             // Update graphics
-            foreach (IQuadtreeObject co in ObjectManager<IQuadtreeObject>.AllObjects) {
+            foreach (IQuadtreeObject co in ObjectManager.AllObjects) {
                 e.Graphics.FillEllipse(co.colour, (float)co.position.x, (float)co.position.y, (float)objectRadius, (float)objectRadius);
             }
             Invalidate();
@@ -88,7 +88,7 @@ namespace ComputingProject_UI
                 Stopwatch sw = Stopwatch.StartNew();
 
                 // Update positions of objects
-                ObjectManager<IQuadtreeObject>.Update(timeController.currentTimeStep, scale, -1);
+                ObjectManager.Update(timeController.currentTimeStep, scale, screen, -1);
                 sw.Stop();
                 int msec = milliseconds - (int)sw.ElapsedMilliseconds;
                 if (msec < 1)
@@ -109,17 +109,18 @@ namespace ComputingProject_UI
 
             centre = new Vector(screenWidthHalf, screenHeightHalf);
 
-            screen = new QuadTree<CelestialObject>(new AABB(centre, centre));
+            screen = new QuadTree<IQuadtreeObject>(new AABB(centre, centre));
+            Console.WriteLine("Screen set!");
         }
 
         void AddObjects() {
-            CelestialObject moon = new CelestialObject("Moon", 1.5E21, 10, 20, new Vector(1000, 700), Brushes.Red, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet = new CelestialObject("Planet", 1E22, 0, 0, new Vector(500, 700), Brushes.Purple, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet1 = new CelestialObject("Planet1", 1E22, 30, 50, new Vector(100, 600), Brushes.Blue, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet2 = new CelestialObject("Planet2", 1E22, 10, 300, new Vector(1200, 100), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet3 = new CelestialObject("Planet3", 1.5E19, 0, 0, new Vector(100, 1000), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet4 = new CelestialObject("Planet4", 1.5E19, 0, 300, new Vector(200, 70), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
-            CelestialObject planet5 = new CelestialObject("Planet5", 1.5E15, 10, 300, new Vector(500, 200), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
+            CelestialObject moon = new CelestialObject("Moon", 1.5E21, 10, 20, new Vector(1000, 0), Brushes.Red, new CircleCollider(new Vector(1, 1), objectRadius));
+            CelestialObject planet = new CelestialObject("Planet", 1E22, -50, 60, new Vector(500, 700), Brushes.Purple, new CircleCollider(new Vector(1, 1), objectRadius));
+            CelestialObject planet1 = new CelestialObject("Planet1", 1E22, 100, 360, new Vector(100, 600), Brushes.Blue, new CircleCollider(new Vector(1, 1), objectRadius));
+            CelestialObject planet2 = new CelestialObject("Planet2", 1E22, 10, 300, new Vector(100, 100), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
+            //CelestialObject planet3 = new CelestialObject("Planet3", 1.5E19, 0, 0, new Vector(100, 1000), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
+            //CelestialObject planet4 = new CelestialObject("Planet4", 1.5E19, 0, 300, new Vector(200, 70), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
+            //CelestialObject planet5 = new CelestialObject("Planet5", 1.5E15, 10, 300, new Vector(500, 200), Brushes.Green, new CircleCollider(new Vector(1, 1), objectRadius));
         }
     }
 }
